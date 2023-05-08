ff4c18767e88fae6e83acbb4bb8cbcb7cbd3930a
ABHIJITH-ANIRUDHAN <abhijithanirudhan@proton.me>
2023-05-08 15:46:06 +0530

    initial level completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Canvas from "./canvas/Canvas"
+import Home from "./pages/Home"
+import Customizer from "./pages/Customizer"
+function App() {
+  return (
+    <>
+    <Home />
+    <Canvas />
+    <Customizer />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { AnimatePresence, motion } from "framer-motion"
+import { useSnapshot } from "valtio"
+import { 
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
